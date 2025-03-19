78b277c389058de5389c1b1d4ddfeefc906e7ece
Fatemeh HajgedaAli <fatemeh.hajgedaali1996@gmail.com>
2025-03-19 11:38:54 +0330

    finish coffee page!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
-document=$
-let clickMenu=$.querySelector('.iconMenu');
+const $=document
+const menu=$.querySelector('.navListOpen');
+const hambergerIcon=$.querySelector('.iconMenu');
+const closeMenu=$.querySelector('#closeMenu');
+//open
+hambergerIcon.addEventListener('click',function(){
+menu.style.display='flex'
+console.log('menu is opened!')
+})
+//close
+closeMenu.addEventListener('click',function(){
+menu.style.display='none'
+console.log('menuBar is closed!')
+})
 
-function 
